2fdcd262d6f38c22bfdc6266f763b1dfb92bf11d
marcni <marcin.radomski89@gmail.com>
2017-08-26 16:48:59 +0200

    add comments to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-var name = prompt('Enter your name');
+/* Welcome alert
+    When you type a name, the word "hello" appears and the entered name
+*/
+var name = prompt('Enter your name'); //writes in the appearing frame
 alert('Hello, ' + name);
-console.log('Hello, ' + name);
+console.log('Hello, ' + name); //script to save the result to the console
