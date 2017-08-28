4125ed2a1a6e1123d5b88caf8010189d2ceb6303
marcni <marcin.radomski89@gmail.com>
2017-08-28 18:40:42 +0200

    add function from task 8.6

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ var name = prompt('Enter your name'); //writes in the appearing frame
 alert('Hello, ' + name);    /* welcome alert */
 console.log('Hello, ' + name); //script to save the result to the console
 
-var a = prompt('podaj liczbę a');
-var h = prompt('podaj liczbę h');
+var a = prompt('give me a number a');
+var h = prompt('give me a number h');
 
 triangleArea = a*h/2;
 
@@ -21,3 +21,25 @@ if (a <= 0 || h <= 0) {
     console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
     alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+    var x = prompt('give me a number x');
+    var y = prompt('give me a number y');
+    var value = (x * x) + (2 * x * y) - (y * y);
+
+    switch (true) {
+        case (value > 0) :
+            alert('Number ' + value + ' is positive');
+            console.log('Number ' + value + ' is positive');
+            break;
+        case (value < 0) :
+            alert('Number ' + value + ' is negative');
+            console.log('Number ' + value + ' is negative');
+            break;
+        case (value === 0) :
+            alert('Your result is ' + value);
+            console.log('Your result is ' + value);
+            break;
+        default:
+            console.log('You have not entered a number. Refresh the page and try again');
+    };
