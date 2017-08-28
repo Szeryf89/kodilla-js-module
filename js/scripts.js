23bfbe208e3253f9f3dcb11e0bff32449dcffb09
marcni <marcin.radomski89@gmail.com>
2017-08-28 18:24:51 +0200

    add tringleArea function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@
 var name = prompt('Enter your name'); //writes in the appearing frame
 alert('Hello, ' + name);    /* welcome alert */
 console.log('Hello, ' + name); //script to save the result to the console
+
+var a = prompt('podaj liczbę a');
+var h = prompt('podaj liczbę h');
+
+triangleArea = a*h/2;
+
+function getTriangleArea(a, h) {
+if (a <= 0 || h <= 0) {
+	return ('Incorrect data');
+	}else
+	return a*h/2;
+	}
+	console.log(getTriangleArea(a, h));
+    console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
