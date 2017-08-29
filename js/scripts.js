0ac02e1b5a9c96cfb53769416056d7568bde3270
marcni <marcin.radomski89@gmail.com>
2017-08-29 17:50:06 +0200

    fixed js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,53 @@
-/* Welcome alert
-    When you type a name, the word "hello" appears and the entered name
-*/
-
-var name = prompt('Enter your name'); //writes in the appearing frame
-alert('Hello, ' + name);    /* welcome alert */
-console.log('Hello, ' + name); //script to save the result to the console
-
-var a = prompt('give me a number a');
-var h = prompt('give me a number h');
-
-triangleArea = a*h/2;
+//function
 
 function getTriangleArea(a, h) {
-if (a <= 0 || h <= 0) {
-	return ('Incorrect data');
-	}else
-	return a*h/2;
-	}
-	console.log(getTriangleArea(a, h));
-    console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
-    alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+        if (a <= 0 || h <= 0) {
+            return ('Incorrect data');
+        }
 
+        return (a * h / 2);
+    }
 
-    var x = prompt('give me a number x');
-    var y = prompt('give me a number y');
-    var value = (x * x) + (2 * x * y) - (y * y);
+    function countShortMultiplicationFormulas(x, y) {
+        var result = (x * x) + (2 * x * y) - (y * y);
 
-    switch (true) {
-        case (value > 0) :
+        /*Check if the result is positive */
+        if (result > 0) {
             alert('Number ' + value + ' is positive');
             console.log('Number ' + value + ' is positive');
-            break;
-        case (value < 0) :
+
+            return;
+        }
+
+        /*Check if the result is negative */
+        if (result < 0) {
             alert('Number ' + value + ' is negative');
             console.log('Number ' + value + ' is negative');
-            break;
-        case (value === 0) :
-            alert('Your result is ' + value);
-            console.log('Your result is ' + value);
-            break;
-        default:
-            console.log('You have not entered a number. Refresh the page and try again');
-    };
+
+            return;
+        }
+
+        //alert if function takes a result of 0
+        alert('Your result is ' + value);
+        console.log('Your result is ' + value);
+    }
+
+    var name = prompt('Enter your name'); //writes in the appearing frame
+    alert('Hello, ' + name);    /* welcome alert */
+    console.log('Hello, ' + name); //script to save the result to the console
+
+    var a = prompt('give me a number a');
+    var h = prompt('give me a number h');
+
+    var triangleArea = a * h / 2;
+
+    console.log(getTriangleArea(a, h));
+
+    console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+    alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+    var x = prompt(' give me a number x');
+    var y = prompt(' give me a number y');
+
+    console.log(countShortMultiplicationFormulas(x, y));
